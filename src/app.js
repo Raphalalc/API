a77611aa3ac9b0df2c4ed4cbce595a9ff80720aa
Raphalalc <raphaelalc19@gmail.com>
2023-01-25 14:52:49 +0100

    add express method

--- src/app.js
+++ src/app.js
@@ -1,28 +1,24 @@
-const http = require('http');
+const express = require('express');
+
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req,res) => {
-    let url = req.url;
+const server = express();
 
-    switch(url) {
-        case '/':
-            res.end('Cosqd')
-        break;
-        case '/posts':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Liste des articles</h1>')
-        break;
-
-        default:
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('Erreur 404')
-        break;
-    }
+server.get ('/', (req, res) => {
+    res.type('html');
+    res.status(200);
+    res.end('Home');
     
 });
 
-server.listen(port, hostname);
+server.get ('/posts', (req, res) => {
+    res.type('html');
+    res.status(200);
+    res.end('Liste des articles');
+});
+
+server.listen(port,hostname , () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
